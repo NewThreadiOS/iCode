1b6eaf5fdba2937ef7150b258506cb5e42a276c5
luojian <prouduct>
2016-10-31 17:36:28 +0800

    登录、注册测试_1
    1.0

---
+++ iCode/LogViewController.m
@@ -0,0 +1,40 @@
+//
+//  LogViewController.m
+//  iCode
+//
+//  Created by Mr.Nut on 16/10/31.
+//  Copyright © 2016年 Mr.Nut. All rights reserved.
+//
+
+#import "LogViewController.h"
+
+@interface LogViewController ()
+
+@end
+
+@implementation LogViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)loginButton:(UIButton *)sender {
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
